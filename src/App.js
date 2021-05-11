6e4384c776260fcbfe8daa8330930740f0bdaeae
greta <gsimonyan228@gmail.com>
2021-05-11 09:47:38 +0300

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import CountrySelect from './components/CountrySelect';
+
+function App(){
+  return (
+    <div className="App">
+      <header className="App-header">
+        <CountrySelect/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
