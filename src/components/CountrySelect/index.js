51ed72dc31dd1e721bb83d9dd66634f47d7feb32
greta <gsimonyan228@gmail.com>
2021-05-11 14:37:02 +0300

    added axios and GET button

--- src/components/CountrySelect/index.js
+++ src/components/CountrySelect/index.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-
+import './style.css';
 import { CountryDropdown, RegionDropdown } from 'react-country-region-selector';
-
+import axios from 'axios';
 
 class CountrySelect extends Component {
   constructor (props) {
     super(props);
-    this.state = { country: '', region: '' };
+    this.state = { 
+      place: {
+        country: 'Armenia', 
+        region: 'Yerevan',
+      },
+      teperature: '',
+      desc: '',
+    };
   }
 
   selectCountry (val) {
@@ -17,18 +24,58 @@ class CountrySelect extends Component {
     this.setState({ region: val });
   }
 
+  componentDidMount(){
+    this.getWeather(this.state.region)
+  }
+
+  getWeather(region){
+    // console.log(region);
+    // console.log(window)
+
+    axios({
+      method: "GET",
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${region}&appid=2aeac406d27d1caf020cdd5e447d1bab`,
+    })
+      .then((response) => {
+        // console.log(response.data.main.temp);
+
+        // Kelvin to Celsius
+        this.setState({ teperature: response.data.main.temp - 273.15 })
+        // console.log(response.data);
+
+        this.setState({desc: response.data.weather[0].main})
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
   render () {
-    const { country, region } = this.state;
+    const { country, region , teperature, desc} = this.state;
     return (
+      <>      
         <div>
-            <CountryDropdown
+          <CountryDropdown
             value={country}
-            onChange={(val) => this.selectCountry(val)} />
-            <RegionDropdown
+            onChange={(val) => this.selectCountry(val)} 
+          />
+          <RegionDropdown
             country={country}
             value={region}
-            onChange={(val) => this.selectRegion(val)} />
+            onChange={(val) => this.selectRegion(val)} 
+          />
         </div>
+        <button onClick={() => this.getWeather(region)}>
+          GET
+        </button>
+      
+        <div className='view'>
+            {new Date().toLocaleString()}
+            <br />
+            {region}, {country} Weather
+            <br />
+            {Math.round(teperature * 100) / 100} ℃ - {desc}
+        </div>
+      </>
     );
   }
 }
