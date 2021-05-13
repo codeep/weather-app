0006f8e2dbce20ab98b5e3b675badba445f9e637
greta <gsimonyan228@gmail.com>
2021-05-13 18:17:40 +0300

    added function of changing the type of time

--- src/components/View/index.js
+++ src/components/View/index.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import './style.css';
+import { OpenWeatherIconURL } from '../../utils';
 
-function View ({ region, country, teperature, description, feels_like, temp_min, temp_max, humidity }){
+function View ({ value, teperature, description, feels_like, temp_min, temp_max, humidity, icon }){
+  
+  function getTepmerature(){
+    let temp = teperature;
+    if (value === 'celsius'){
+      temp =  teperature - 273.15
+    }else if(value === 'fahrenheit'){
+      temp = (teperature - 273.15) * 1.8 + 32
+    }
+    return Math.round(temp * 100) / 100;
+  }
     return (
       <>
         <div className='view'>
           <div>
-            {region}, {country} Weather
+            Weather
             <br />
-            {teperature} °K
+            {getTepmerature()}
             <br />
             {description}
           </div>
           <div>
-            {new Date().toLocaleString()}
-            <br />
-            {temp_min} / {temp_max}
+            <img src={OpenWeatherIconURL+icon+'@2x.png'} alt='weather_icon'/>
+            <div>
+              {new Date().toDateString()}
+              <br />
+              {temp_min} / {temp_max}
+            </div>
           </div>
         </div>
         <div>
