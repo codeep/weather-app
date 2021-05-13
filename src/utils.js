e2580f152f4b2792f7451335ba0eec75f241326e
greta <gsimonyan228@gmail.com>
2021-05-13 18:03:35 +0300

    added url

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+export const IPINFO_API = 'https://ipinfo.io/?token=6196c9fe7a1737';
+export const OpenWeatherIconURL = 'http://openweathermap.org/img/wn/';
 const API_KEY = '2aeac406d27d1caf020cdd5e447d1bab';
 const OpenWeatherURL = 'https://api.openweathermap.org/data/2.5/weather';
 
