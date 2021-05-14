9fac9697dbe87a05a78b0b1a156a26b7ae792a35
greta <gsimonyan228@gmail.com>
2021-05-14 09:37:27 +0300

    added prop-types

--- src/components/TypeSelect/index.js
+++ src/components/TypeSelect/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function TypeSelect({getTepmeratureType, value}){
-    return(
-      <div>
-        <form>
-          <label> Choose a type </label>
-          <select onChange={getTepmeratureType} value={value}>
-            <option value='kelvin'>Kelvin</option>
-            <option value='fahrenheit'>Fahrenheit</option>
-            <option value='celsius'>Celsius</option>
-          </select>
-        </form>
-      </div>
-    )
+  return(
+    <>
+      <form>
+        <label> Choose a type </label>
+        <select onChange={getTepmeratureType} value={value}>
+          <option value='kelvin'>Kelvin</option>
+          <option value='fahrenheit'>Fahrenheit</option>
+          <option value='celsius'>Celsius</option>
+        </select>
+      </form>
+    </>
+  )
 }
+
+TypeSelect.propTypes = {
+  getTepmeratureType: PropTypes.func.isRequired,
+};
 
 export default TypeSelect
