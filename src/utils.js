ed1bbe419d46ce4e0e7e4134ba70e27c29c10265
greta <gsimonyan228@gmail.com>
2021-05-14 10:05:19 +0300

    changed the function to arrowFunc

--- src/utils.js
+++ src/utils.js
@@ -4,19 +4,19 @@ const API_KEY = '2aeac406d27d1caf020cdd5e447d1bab';
 const OpenWeatherURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 export const getOpenweatherUrl = (region) => (
-    `${OpenWeatherURL}?q=${region}&appid=${API_KEY}`
+  `${OpenWeatherURL}?q=${region}&appid=${API_KEY}`
 )
 
 export const setOpenWeatherIcon = (icon) => {
-   return <img src={OpenWeatherIconURL+icon+'@2x.png'} alt='weather_icon'/>
+  return <img src={OpenWeatherIconURL+icon+'@2x.png'} alt='weather_icon'/>
 }
 
-export const setTepmerature = function(teperature, value){
-    let temp = teperature;
-    if (value === 'celsius'){
-      temp =  teperature - 273.15
-    }else if(value === 'fahrenheit'){
-      temp = (teperature - 273.15) * 1.8 + 32
-    }
-    return Math.round(temp * 100) / 100;
+export const setTepmerature = (teperature, value) => {
+  let temp = teperature;
+  if (value === 'celsius'){
+    temp =  teperature - 273.15
+  }else if(value === 'fahrenheit'){
+    temp = (teperature - 273.15) * 1.8 + 32
   }
+  return Math.round(temp * 100) / 100;
+}
