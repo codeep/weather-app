2ae3c18df829797454b080b8c8de628a1e054689
greta <gsimonyan228@gmail.com>
2021-05-14 09:40:07 +0300

    moved function to utils file

--- src/components/View/index.js
+++ src/components/View/index.js
@@ -1,33 +1,23 @@
 import React from 'react';
 import './style.css';
-import { OpenWeatherIconURL } from '../../utils';
+import { setTepmerature, setOpenWeatherIcon } from '../../utils';
 
 function View ({ value, teperature, description, feels_like, temp_min, temp_max, humidity, icon }){
-  
-  function getTepmerature(){
-    let temp = teperature;
-    if (value === 'celsius'){
-      temp =  teperature - 273.15
-    }else if(value === 'fahrenheit'){
-      temp = (teperature - 273.15) * 1.8 + 32
-    }
-    return Math.round(temp * 100) / 100;
-  }
     return (
       <>
         <div className='view'>
           <div>
             Weather
-            <br />
-            {getTepmerature()}
+            <br/>
+            {setTepmerature(teperature,value)}
             <br />
             {description}
           </div>
           <div>
-            <img src={OpenWeatherIconURL+icon+'@2x.png'} alt='weather_icon'/>
+            {setOpenWeatherIcon(icon)}
             <div>
               {new Date().toDateString()}
-              <br />
+              <br/>
               {temp_min} / {temp_max}
             </div>
           </div>
