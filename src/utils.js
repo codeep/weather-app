3eec7e905b0560db451d7ef43bef166de94d0b61
greta <gsimonyan228@gmail.com>
2021-05-14 09:36:49 +0300

    added functions

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,22 @@
 export const IPINFO_API = 'https://ipinfo.io/?token=6196c9fe7a1737';
-export const OpenWeatherIconURL = 'http://openweathermap.org/img/wn/';
+const OpenWeatherIconURL = 'http://openweathermap.org/img/wn/';
 const API_KEY = '2aeac406d27d1caf020cdd5e447d1bab';
 const OpenWeatherURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 export const getOpenweatherUrl = (region) => (
     `${OpenWeatherURL}?q=${region}&appid=${API_KEY}`
 )
+
+export const setOpenWeatherIcon = (icon) => {
+   return <img src={OpenWeatherIconURL+icon+'@2x.png'} alt='weather_icon'/>
+}
+
+export const setTepmerature = function(teperature, value){
+    let temp = teperature;
+    if (value === 'celsius'){
+      temp =  teperature - 273.15
+    }else if(value === 'fahrenheit'){
+      temp = (teperature - 273.15) * 1.8 + 32
+    }
+    return Math.round(temp * 100) / 100;
+  }
