5b184af9dc4b747014dc8710e25530e0bf6dcf28
greta <gsimonyan228@gmail.com>
2021-05-20 12:09:45 +0300

    added TypeSelect, close button and getTempType foo

--- src/components/View/index.js
+++ src/components/View/index.js
@@ -1,45 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.css';
 import PropTypes from 'prop-types';
-import { setTepmerature, setOpenWeatherIcon } from '../../utils';
+import TypeSelect from '../TypeSelect';
+import { setTemperature, setOpenWeatherIcon } from '../../utils';
 
-function View ({ value, teperature, description, feels_like, temp_min, temp_max, humidity, icon }){
+class View extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+    };
+  };
+
+  getTemperatureType = (event) => {
+    this.setState({
+      value: event.target.value,
+    });
+  };
+
+  render() {
+    const { data: { region, temp, description, feels_like, temp_min, temp_max, humidity, icon } } = this.props;
+    const { value } = this.state;
+
     return (
-      <>
+      <div className='view_board'>
+        <button onClick={this.props.removeView}> Close </button>
+        <TypeSelect getTemperatureType={this.getTemperatureType} value={value} />
         <div className='view'>
           <div>
+            {region}
+            <br />
             Weather
-            <br/>
-            {setTepmerature(teperature,value)}
+            <br />
+            {setTemperature(temp, value)}
             <br />
             {description}
           </div>
           <div>
-            <img src={setOpenWeatherIcon(icon)} alt='weather_icon'/>
+            <img src={setOpenWeatherIcon(icon)} alt='weather_icon' />
             <div>
-              {new Date().toDateString()}
-              <br/>
-              {setTepmerature(temp_min,value)} / {setTepmerature(temp_max,value)}
+              {new Date().toLocaleString()}
+              <br />
+              {setTemperature(temp_min, value)} / {setTemperature(temp_max, value)}
             </div>
           </div>
         </div>
         <div>
-          Feels Like- {setTepmerature(feels_like,value)}
-          <br/>
+          Feels Like- {setTemperature(feels_like, value)}
+          <br />
           Humidity - {humidity}
         </div>
-      </>
-  );
+      </div>
+    );
+  }
 }
 
 View.propTypes = {
-  value: PropTypes.string.isRequired,
-  teperature: PropTypes.number.isRequired, 
-  description: PropTypes.string.isRequired, 
-  feels_like: PropTypes.number.isRequired, 
-  temp_min: PropTypes.number.isRequired, 
-  temp_max: PropTypes.number.isRequired, 
-  humidity: PropTypes.number.isRequired, 
-  icon: PropTypes.string.isRequired
+  data: PropTypes.shape({
+    temp: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    feels_like: PropTypes.number.isRequired,
+    temp_min: PropTypes.number.isRequired,
+    temp_max: PropTypes.number.isRequired,
+    humidity: PropTypes.number.isRequired,
+    icon: PropTypes.string.isRequired
+  }).isRequired,
+  removeView: PropTypes.func.isRequired,
 };
 export default View
